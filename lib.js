61b8cff0a197c2c8a4874751108e3f6e7abffefc
Anthony Perry <atperry7@gmail.com>
2017-06-09 09:33:31 -0500

    Experimented with a flatten method for functional programming that I found, works excellent, helps show functional on a different level

--- lib.js
+++ lib.js
@@ -35,6 +35,17 @@ const listingsFilter =
     itemName =>
           listings.filter((list) => list.name === itemName)
 
+          // concat :: [a] -> [a] -> [a]
+          const concat = x => y => x.concat(y)
+
+          // concatMap :: (a -> [b]) -> [a] -> [b]
+          const concatMap = f => xs => xs.map(f).reduce((x, y) => concat(x)(y), [])
+
+          // id :: a -> a
+          const id = x => x
+
+          // flatten :: [[a]] -> [a]
+          const flatten = concatMap(id)
 /**
  * should return an array of carts with each given customer's shopping list
  * as an array of items
@@ -43,13 +54,14 @@ const constructCarts =
   listings =>
     customers => {
       const arr = customers.map(cust => {
-        const shopping = entries(cust.shoppingList)
-      return { customer: cust.name, items: shopping.map((item) => { return itemRepeater(item[0])(item[1]) }) }
-      })
+        const shopping = entries(cust.shoppingList).map((item) => { return itemRepeater(item[0])(item[1]) })
+
+        return { customer: cust.name, items: flatten(shopping)} })
 
       return arr
       // const test = [(customers[0].shoppingList)] // Destructs the Customer Shopping List into an array
     }
+
 
 module.exports = {
   listing,
