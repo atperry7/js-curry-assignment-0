c9c9fa13e15e3c2071ff4ee041c88c413638370e
Anthony Perry <atperry7@gmail.com>
2017-06-09 09:43:07 -0500

    Cleaned up spacing for syntax

--- lib.js
+++ lib.js
@@ -35,17 +35,17 @@ const listingsFilter =
     itemName =>
           listings.filter((list) => list.name === itemName)
 
-          // concat :: [a] -> [a] -> [a]
-          const concat = x => y => x.concat(y)
+// concat :: [a] -> [a] -> [a]
+const concat = x => y => x.concat(y)
 
-          // concatMap :: (a -> [b]) -> [a] -> [b]
-          const concatMap = f => xs => xs.map(f).reduce((x, y) => concat(x)(y), [])
+// concatMap :: (a -> [b]) -> [a] -> [b]
+const concatMap = f => xs => xs.map(f).reduce((x, y) => concat(x)(y), [])
 
-          // id :: a -> a
-          const id = x => x
+// id :: a -> a
+const id = x => x
 
-          // flatten :: [[a]] -> [a]
-          const flatten = concatMap(id)
+// flatten :: [[a]] -> [a]
+const flatten = concatMap(id)
 /**
  * should return an array of carts with each given customer's shopping list
  * as an array of items
@@ -56,12 +56,12 @@ const constructCarts =
       const arr = customers.map(cust => {
         const shopping = entries(cust.shoppingList).map((item) => { return itemRepeater(item[0])(item[1]) })
 
-        return { customer: cust.name, items: flatten(shopping)} })
+        return { customer: cust.name, items: flatten(shopping) }
+      })
 
       return arr
       // const test = [(customers[0].shoppingList)] // Destructs the Customer Shopping List into an array
     }
-
 
 module.exports = {
   listing,
