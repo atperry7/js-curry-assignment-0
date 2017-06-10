c0c0becb634df3696ca463d71adb74430ea964c2
Anthony Perry <atperry7@gmail.com>
2017-06-09 19:53:56 -0500

    Cleaned up code

--- lib.js
+++ lib.js
@@ -23,6 +23,15 @@ const cart =
     items
   })
 
+// concat :: [a] -> [a] -> [a]
+const concat = x => y => x.concat(y)
+// concatMap :: (a -> [b]) -> [a] -> [b]
+const concatMap = f => xs => xs.map(f).reduce((x, y) => concat(x)(y), [])
+// id :: a -> a
+const id = x => x
+// flatten :: [[a]] -> [a]
+const flatten = concatMap(id)
+
 /**
  * should return an array with the `itemName` repeated `count` number of times
  */
@@ -35,18 +44,6 @@ const listingsFilter =
     itemName =>
           listings.filter((list) => list.name === itemName)
 
-// concat :: [a] -> [a] -> [a]
-const concat = x => y => x.concat(y)
-
-// concatMap :: (a -> [b]) -> [a] -> [b]
-const concatMap = f => xs => xs.map(f).reduce((x, y) => concat(x)(y), [])
-
-// id :: a -> a
-const id = x => x
-
-// flatten :: [[a]] -> [a]
-const flatten = concatMap(id)
-
 const shoppingListRepeater =
   func =>
     shoppingList => func(shoppingList).map((item) => itemRepeater(item[0])(item[1]))
@@ -58,12 +55,9 @@ const shoppingListRepeater =
 const constructCarts =
   listings =>
     customers => {
-      const arr = customers.map(cust => {
+      return customers.map(cust => {
         return { customer: cust.name, items: flatten(shoppingListRepeater(entries)(cust.shoppingList)) }
       })
-
-      return arr
-      // const test = [(customers[0].shoppingList)] // Destructs the Customer Shopping List into an array
     }
 
 module.exports = {
